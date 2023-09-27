657daff2f2c875ca2a0e39ba827b2b2ebc373274
adegard <annaelle.degard@laposte.net>
2023-09-27 14:57:07 +0200

    add time after spawn bomb

--- Assets/Script/Annaelle/RayonCollider.cs
+++ Assets/Script/Annaelle/RayonCollider.cs
@@ -6,7 +6,21 @@ using UnityEngine;
 
 public class RayonCollider : MonoBehaviour
 {
-    
+    private CapsuleCollider cc;
+
+    private void Start()
+    {
+        cc = this.GetComponent<CapsuleCollider>();
+        StartCoroutine(ControlCollider());
+    }
+
+    IEnumerator ControlCollider()
+    {
+        cc.enabled = false;
+        yield return new WaitForSeconds(1.5f);
+        cc.enabled = true;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "MurDestructible" || collision.transform.tag == "Player")
