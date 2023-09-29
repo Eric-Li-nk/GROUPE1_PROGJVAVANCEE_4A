f78ee19f2ffd73ccef31b26311bf2b87ddf8366d
Eric-Li-nk <73618843+Eric-Li-nk@users.noreply.github.com>
2023-09-29 12:24:53 +0200

    update: map is now tracked of mcts

--- Assets/Scripts/Game/GameInitialization.cs
+++ Assets/Scripts/Game/GameInitialization.cs
@@ -19,10 +19,16 @@ public class GameInitialization : MonoBehaviour
     [SerializeField] private Transform spawnPointPlayer1;
     [SerializeField] private Transform spawnPointPlayer2;
 
+    private GameObject[][] mapObjet;
+    private char[][] map;
+    private string filename;
+
     void Start()
     {
-        SpawnBlocs();
+        SelectMap();
+        IntiateMap();
         SpawnPlayers();
+        GenerateMap();
     }
 
     private void SpawnPlayers()
@@ -36,6 +42,101 @@ public class GameInitialization : MonoBehaviour
         {
             GameManager.instance.player1 = SpawnPlayer(_gameConfig.player1, spawnPointPlayer1);
             GameManager.instance.player2 = SpawnPlayer(_gameConfig.player2, spawnPointPlayer2);
+        }
+    }
+
+    private IEnumerator DestroyBlocs()
+    {
+        foreach (var row in mapObjet)
+        {
+            foreach (var bloc in row)
+            {
+                Destroy(bloc);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+    
+    private void IntiateMap(){
+        var reader = new  StreamReader(filename);
+        map = new char[15][];
+        string line;
+        int row=0;
+        while (true)
+        {
+            line=reader.ReadLine();
+            
+            if (line==null) break;
+            char[] lineArray = line.ToCharArray();
+            map[row] = new char[25];
+
+            for (int col = 0; col < 25; col++)
+            {
+                map[row][col] = lineArray[col];
+            }
+
+            row++;
+
+        }
+        reader.Close();
+    }
+
+    private void GenerateMap()
+    {
+        char caseMap;
+        int i = 0;
+        mapObjet = new GameObject[15][];
+        for(int row = 0; row < 15; row++)
+        {
+            mapObjet[row] = new GameObject[25];
+            for(int col = 0; col < 25; col++)
+            {
+                caseMap = map[row][col];
+                switch(caseMap)
+                {
+                    case '0':
+                        Vector3 position = new Vector3(col-1, blocPrefab.transform.position.y,  -row + 1);
+                        mapObjet[row][col] = Instantiate(blocPrefab, position, blocPrefab.transform.rotation, blocList);;
+                        i++;
+                        break;
+                    case 'M':
+                        mapObjet[row][col] = null;
+                        break;
+                    case 'L':
+                        mapObjet[row][col] = null;
+                        break;
+                    case '1':
+                        mapObjet[row][col] = GameManager.instance.player1;
+                        break;
+                    case '2':
+                        mapObjet[row][col] = GameManager.instance.player2;
+                        break;
+                    case 'A':
+                        mapObjet[row][col] = GameManager.instance.player1;
+                        break;
+                    case 'B':
+                        mapObjet[row][col] = GameManager.instance.player2;
+                        break;
+                }
+            }
+        }
+    }
+
+    private void SelectMap()
+    {
+        PlayerType joueur1 = _gameConfig.player1;
+        PlayerType joueur2 = _gameConfig.player2;
+        if(joueur1 == PlayerType.Human && joueur2 == PlayerType.Human){
+            filename = "Assets/Map/MapPVP.txt";
+        }
+        else if((joueur1 == PlayerType.Human && joueur2 == PlayerType.Random) || (joueur1 == PlayerType.Human && joueur2 == PlayerType.MCTS)){
+            filename = "Assets/Map/MPvIA.txt";
+        }
+        else if((joueur1 == PlayerType.Random && joueur2 == PlayerType.Random) || (joueur1 == PlayerType.Random && joueur2 == PlayerType.MCTS) || (joueur1 == PlayerType.MCTS && joueur2 == PlayerType.MCTS) || (joueur1 == PlayerType.MCTS && joueur2 == PlayerType.Random)){
+            filename = "Assets/Map/MapIAvIA.txt";
+        }
+        else{
+            filename = "Assets/Map/Map1.txt";
         }
     }
 
@@ -58,35 +159,5 @@ public class GameInitialization : MonoBehaviour
                 break;
         }
         return Instantiate(prefabToSpawn, transform.position, transform.rotation);
-    }
-
-    private void SpawnBlocs()
-    {
-        string fileName = "Assets/Map/map1.txt";
-        StreamReader reader = new StreamReader(fileName);
-        char c = (char) reader.Read();
-        int x = -1, z = 1;
-        while (!reader.EndOfStream)
-        {
-            c = (char) reader.Read();
-            
-            Vector3 position = new Vector3(x, blocPrefab.transform.position.y, z);
-            switch (c)
-            {
-                case '0':
-                    Instantiate(blocPrefab, position, blocPrefab.transform.rotation, blocList);
-                    x += 1;
-                    break;
-                case '\n':
-                    x = -1;
-                    z -= 1;
-                    break;
-                default:
-                    x += 1;
-                    break;
-            }
-        }
-        reader.Close();
-
     }
 }
