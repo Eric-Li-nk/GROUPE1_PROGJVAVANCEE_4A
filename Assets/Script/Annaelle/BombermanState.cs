b461c6307eb99da207f6b7d2e32dff53f9998698
Tristan Blin <70915237+TristanBlin@users.noreply.github.com>
2023-09-29 21:14:37 +0200

    Update commentaire BombermanState.cs

--- Assets/Script/Annaelle/BombermanState.cs
+++ Assets/Script/Annaelle/BombermanState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BombermanState : MonoBehaviour
 {
+    // Enumeration des actions que le joueur et l'ia peuvent effectuer
     public enum PlayerAction{
         DoNothing = 0,
         GoLeft = 1,
